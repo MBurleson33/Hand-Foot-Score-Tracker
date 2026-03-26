c9c931dc9a54f9b1ff834162235e41a9781c405e
MBurleson33 <114445009+MBurleson33@users.noreply.github.com>
2026-03-26 13:28:53 -0500

    Create sw.js

---
+++ sw.js
@@ -0,0 +1,24 @@
+const CACHE = 'handf-v1';
+const ASSETS = [
+  '/Hand-Foot-Score-Tracker/',
+  '/Hand-Foot-Score-Tracker/index.html',
+  '/Hand-Foot-Score-Tracker/manifest.json'
+];
+
+self.addEventListener('install', e => {
+  e.waitUntil(caches.open(CACHE).then(c => c.addAll(ASSETS)));
+  self.skipWaiting();
+});
+
+self.addEventListener('activate', e => {
+  e.waitUntil(caches.keys().then(keys =>
+    Promise.all(keys.filter(k => k !== CACHE).map(k => caches.delete(k)))
+  ));
+  self.clients.claim();
+});
+
+self.addEventListener('fetch', e => {
+  e.respondWith(
+    caches.match(e.request).then(cached => cached || fetch(e.request))
+  );
+});
